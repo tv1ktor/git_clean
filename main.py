aaf5879b486359abe208e0b46cbc339bf628db11
tv1ktor <vt6x@uvawise.edu>
2019-01-18 02:54:24 +0200

    script finished

--- main.py
+++ main.py
@@ -10,20 +10,19 @@ stdoutput, stderroutput = git_brunches.communicate()
 if stdoutput:
 	branches = re.split(r"\s+|\n|[\s\*]", stdoutput)
 	branches = list(filter(None, branches))
-	print("Found {} branches.".format(len(branches)))
-
+	print("<< Found {} branches:".format(len(branches)))
 	for branch in branches:
 		if branch == 'master':
 			print("\033[92m" + branch, "(safe)\033[0m")
 		else:
 			print(branch)
 else:
-	print("Output from subprocess not received")
+	print("<< Output from subprocess not received")
 	exit()
 
 try:
 	while True:
-		command = input('Delete all fully merged branches [y/n]: ')
+		command = input('\n>> Delete all fully merged branches [y/n]: ')
 		if command == 'y':
 			from script import init_safe_branch_clean
 			branches.remove('master')
@@ -31,9 +30,9 @@ try:
 		elif command == 'n':
 			raise KeyboardInterrupt
 		else: 
-			print("Given wrong command.")
+			print("<< Given wrong command.")
 
 except KeyboardInterrupt as error:
-	print('Exiting the program...')
+	print('<< Exiting the program...')
 	exit()
 	
--- script.py
+++ script.py
@@ -1,18 +1,25 @@
 import subprocess
+import progressbar
 from sys import exit
+from time import sleep
 
 
 def init_safe_branch_clean(branches):
+	remaining_branches = []
 	PIPE = subprocess.PIPE
+	bar = progressbar.ProgressBar(maxval=len(branches), \
+		widgets=[progressbar.Bar('|', '[', ']'), ' ', progressbar.Percentage()])
+	
+	bar.start()
 	
 	for branch in branches:
 		deleted_branch = subprocess.Popen(['git', 'branch', '-d', branch], stdout=PIPE, stderr=PIPE, text=True)
 		stdoutput, stderroutput = deleted_branch.communicate()
-		print(stdoutput)
-		print(stderroutput)
-		exit()
+		bar.update(i+1)
+		sleep(0.1)
+		if stderroutput:
+			remaining_branches.push(deleted_branch)
+			continue
 
-
-
-
-
+	bar.finish()
+	exit()
